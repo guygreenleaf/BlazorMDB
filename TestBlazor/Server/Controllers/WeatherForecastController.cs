27ed87b5b29509b1653008f92288f8bc8512adaa
guygreenleaf <guygreenleaf@icloud.com>
2022-10-24 22:39:31 -0700

    testv

--- TestBlazor/Server/Controllers/WeatherForecastController.cs
+++ TestBlazor/Server/Controllers/WeatherForecastController.cs
@@ -19,16 +19,16 @@ namespace TestBlazor.Server.Controllers
             _logger = logger;
         }
 
-        [HttpGet]
-        public IEnumerable<WeatherForecast> Get()
-        {
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-            {
-                Date = DateTime.Now.AddDays(index),
-                TemperatureC = Random.Shared.Next(-20, 55),
-                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-            })
-            .ToArray();
-        }
+        // [HttpGet]
+        // public IEnumerable<WeatherForecast> Get()
+        // {
+        //     return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+        //     {
+        //         Date = DateTime.Now.AddDays(index),
+        //         TemperatureC = Random.Shared.Next(-20, 55),
+        //         Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+        //     })
+        //     .ToArray();
+        // }
     }
 }
